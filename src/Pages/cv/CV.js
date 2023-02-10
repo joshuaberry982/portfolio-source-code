a4c4844bfe968312d08302507cb3ad3a0eb20843
Joshua Berry <121140587+joshuaberry982@users.noreply.github.com>
2023-02-10 01:53:34 +0000

    finished cv

--- src/Pages/cv/CV.js
+++ src/Pages/cv/CV.js
@@ -96,11 +96,13 @@ export default function CV() {
 
             {/* Right */}
             <div className="right1">
-            <div class="about1">
-                    <h3>Profile</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor autem est reprehenderit ullam possimus! Itaque quidem nostrum dolores fugiat mollitia, blanditiis facere, necessitatibus ipsa, et laudantium quae atque quam sint? Lorem ipsum dolor sit, amet consectetur adipisicing elit. A inventore vel delectus! Quo doloremque culpa nisi illum tempora vero adipisci nulla, cumque totam, sapiente unde. Molestias id illum iure est?<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolore quia totam ducimus corporis sit repellat quaerat incidunt, ab reprehenderit debitis consequuntur asperiores numquam vitae eveniet deserunt et dolor distinctio.</p>
-                 </div>
+                <div class="about1">
+                <h3>Profile</h3>
+                    <p>I have been working with food for around 6 years starting May 2017. I began working in a takeaway which I began managing at the start of the Coronavirus Pandemic, while I was in college studying Professional Cookery. Since gaining my qualification, I have working in various hotels and cooked for many people including HNWIs and football teams.
+                    <br /><br />
+                    As much as I have enjoyed my career so far, I have always felt like I should be doing something that has a more lasting impact, and then I stumbled across software engineering. I began watching YouTube 'Crash Courses', practicing what I had learned and eventually started studying with Codecademy. My tech stack includes HTML5, CSS3, JavaScript, React, Redux, Express, Node and PostgreSQL. I also intend to keep studying and develop a niche in React and Redux</p>
                  <br />
+                 </div>
                  <div class="about1">
                     <h3>Experience</h3>
                     <div class="box1">
@@ -110,7 +112,7 @@ export default function CV() {
                         </div>
                         <div class="text1">
                             <h4>Team Member/Manager</h4>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, totam iusto saepe magni nemo reprehenderit accusantium non aspernatur quis delectus culpa rem deserunt? Incidunt fugit quae soluta dolorum facere earum?</p>
+                            <p>I started working as a team member when I was 14 years old. I quickly learned how to do each section and became a pizza chef. When the Coronavirus Pandemic hit, I became manager and led the team to be very successful while being one of the very few food vendors still open. I also cooked for homeless people and struggling families during this time.</p>
                         </div>
                     </div>
                     <br />
@@ -121,13 +123,13 @@ export default function CV() {
                         </div>
                         <div class="text1">
                             <h4>Chef De Partie</h4>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, totam iusto saepe magni nemo reprehenderit accusantium non aspernatur quis delectus culpa rem deserunt? Incidunt fugit quae soluta dolorum facere earum?</p>
+                            <p>I worked across several hotels (mostly 4 star) including MacDonald Hotels and Bolton Stadium Hotel. During this time, I cooked for football teams such as Bolton Wanderers, Bristol Rovers and Crystal Palace Ladies, aswell as High Net Worth Individuals and the hotel restaurants.</p>
                         </div>
                     </div>
-                 </div>
-                 <br />
+                </div>
+                <br />
 
-                 <div class="about1 skills1">
+                <div class="about1 skills1">
                     <h3>Professional Skills</h3>
                     <div class="box1">
                         <h4>HTML</h4>
@@ -165,7 +167,7 @@ export default function CV() {
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                 </div>
+                </div>
             </div>
         </div>
         <div className="margin1"></div>
