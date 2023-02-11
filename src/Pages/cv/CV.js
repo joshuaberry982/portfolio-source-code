d9bafbad2f92abdb9c7facbe35a94ccdcff3a86c
Joshua Berry <121140587+joshuaberry982@users.noreply.github.com>
2023-02-11 23:46:47 +0000

    fixed cv

--- src/Pages/cv/CV.js
+++ src/Pages/cv/CV.js
@@ -96,32 +96,32 @@ export default function CV() {
 
             {/* Right */}
             <div className="right1">
-                <div class="about1">
+                <div className="about1">
                 <h3>Profile</h3>
                     <p>I have been working with food for around 6 years starting May 2017. I began working in a takeaway which I began managing at the start of the Coronavirus Pandemic, while I was in college studying Professional Cookery. Since gaining my qualification, I have working in various hotels and cooked for many people including HNWIs and football teams.
                     <br /><br />
-                    As much as I have enjoyed my career so far, I have always felt like I should be doing something that has a more lasting impact, and then I stumbled across software engineering. I began watching YouTube 'Crash Courses', practicing what I had learned and eventually started studying with Codecademy. My tech stack includes HTML5, CSS3, JavaScript, React, Redux, Express, Node and PostgreSQL. I also intend to keep studying and develop a niche in React and Redux</p>
+                    As much as I have enjoyed my career so far, I have always felt like I should be doing something that has a more lasting impact, and then I stumbled across software engineering. I began watching YouTube 'Crash Courses', practicing what I had learned and eventually started studying with Codecademy. My tech stack includes HTML5, CSS3, JavaScript, React, Redux, Express, Node and PostgreSQL. I also intend to keep studying and develop a niche in React and Redux.</p>
                  <br />
                  </div>
-                 <div class="about1">
+                 <div className="about1">
                     <h3>Experience</h3>
-                    <div class="box1">
-                        <div class="year-company1">
+                    <div className="box1">
+                        <div className="year-company1">
                             <h5>2017-2021</h5>
                             <h5>Picasso Pizzeria</h5>
                         </div>
-                        <div class="text1">
+                        <div className="text1">
                             <h4>Team Member/Manager</h4>
                             <p>I started working as a team member when I was 14 years old. I quickly learned how to do each section and became a pizza chef. When the Coronavirus Pandemic hit, I became manager and led the team to be very successful while being one of the very few food vendors still open. I also cooked for homeless people and struggling families during this time.</p>
                         </div>
                     </div>
                     <br />
-                    <div class="box1">
-                        <div class="year-company1">
+                    <div className="box1">
+                        <div className="year-company1">
                             <h5>2021-2023</h5>
                             <h5>Various Hotels</h5>
                         </div>
-                        <div class="text1">
+                        <div className="text1">
                             <h4>Chef De Partie</h4>
                             <p>I worked across several hotels (mostly 4 star) including MacDonald Hotels and Bolton Stadium Hotel. During this time, I cooked for football teams such as Bolton Wanderers, Bristol Rovers and Crystal Palace Ladies, aswell as High Net Worth Individuals and the hotel restaurants.</p>
                         </div>
@@ -129,45 +129,46 @@ export default function CV() {
                 </div>
                 <br />
 
-                <div class="about1 skills1">
+                <div className="about1 skills1">
                     <h3>Professional Skills</h3>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>HTML</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>CSS</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>JavaScript</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>PostgreSQL</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>Redux</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
-                    <div class="box1">
+                    <div className="box1">
                         <h4>API Development</h4>
-                        <span class="percent1">
+                        <span className="percent1">
                             <div style={{ width: "85%" }}></div>
                         </span>
                     </div>
                 </div>
+                
             </div>
         </div>
         <div className="margin1"></div>
